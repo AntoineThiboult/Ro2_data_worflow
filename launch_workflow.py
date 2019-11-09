551547ab14f2d1aa01703e57f8e3e8480538c972
AntoineThiboult <antoine.thiboult.1@ulaval.ca>
2019-11-08 21:31:19 -0500

    Add module to merge eddy and slow data

--- launch_workflow.py
+++ launch_workflow.py
@@ -1,22 +1,31 @@
 import os
-from joblib import Parallel, delayed
+#from joblib import Parallel, delayed
 import process_ro2_data as prd
 
 listStations    = ["Berge","Reservoir","Foret_ouest","Foret_est","Foret_sol"]
-rawFileDir      =os.path.join("C:\\","Users","anthi182","Desktop","Data_for_automatization","Raw_data") 
+rawFileDir      =os.path.join("C:\\","Users","anthi182","Desktop","Data_for_automatization","Raw_data")
 asciiOutDir     =os.path.join("C:\\","Users","anthi182","Desktop","Data_for_automatization","Ascii_data")
 
 #% Bin to ascii
-Parallel(n_jobs=5)(delayed(prd.convert_CSbinary_to_csv)(iStation,rawFileDir,asciiOutDir) for iStation in listStations)    
+#Parallel(n_jobs=5)(delayed(prd.convert_CSbinary_to_csv)(iStation,rawFileDir,asciiOutDir) for iStation in listStations)
 #for iStation in listStations:
 #    prd.convert_CSbinary_to_csv(iStation,rawFileDir,asciiOutDir)
 
-   
+
 #% Ascii to eddypro
-eddyproOutDir   ="C:/Users/anthi182/Desktop/Data_for_automatization/Eddypro_data/"
-eddyproConfig   ="C:/Users/anthi182/Documents/GitHub/Ro2_data_worflow/EddyProConfig/"
-eddyproMetaData ="C:/Users/anthi182/Documents/GitHub/Ro2_data_worflow/EddyProConfig/"
-asciiOutDir     ="C:/Users/anthi182/Desktop/Data_for_automatization/Ascii_data/"
-Parallel(n_jobs=5)(delayed(prd.batch_process_eddypro)(iStation,asciiOutDir,eddyproConfig,eddyproMetaData,eddyproOutDir) for iStation in listStations) 
-#for iStation in listStations:        
+#listStations    = ["Berge","Reservoir","Foret_ouest","Foret_est"]
+#eddyproOutDir   ="C:/Users/anthi182/Desktop/Data_for_automatization/Eddypro_data/"
+#eddyproConfig   ="C:/Users/anthi182/Documents/GitHub/Ro2_data_worflow/EddyProConfig/"
+#eddyproMetaData ="C:/Users/anthi182/Documents/GitHub/Ro2_data_worflow/EddyProConfig/"
+#asciiOutDir     ="C:/Users/anthi182/Desktop/Data_for_automatization/Ascii_data/"
+#Parallel(n_jobs=5)(delayed(prd.batch_process_eddypro)(iStation,asciiOutDir,eddyproConfig,eddyproMetaData,eddyproOutDir) for iStation in listStations)
+#for iStation in listStations:
 #    prd.batch_process_eddypro(iStation,asciiOutDir,eddyproConfig,eddyproMetaData,eddyproOutDir)
+
+#% Merge eddy data and slow data
+listStations    = ["Foret_est"]#["Berge","Reservoir","Foret_ouest","Foret_est"]
+asciiOutDir     =os.path.join("Data_for_tests","Ascii_data")
+eddyproOutDir   =os.path.join("Data_for_tests","Eddypro_data")
+mergedCsvOutDir =os.path.join("Data_for_tests","Merged_csv")
+for iStation in listStations:
+    prd.merge_eddy_and_slow(iStation,asciiOutDir,eddyproOutDir,mergedCsvOutDir)
--- process_ro2_data.py
+++ process_ro2_data.py
@@ -11,44 +11,45 @@ import subprocess
 import shutil
 import fileinput
 from datetime import datetime as dt #TODO , timedelta as td
+from glob import glob
 
 def convert_CSbinary_to_csv(stationName,rawFileDir,asciiOutDir):
 
     #Find folders that match the pattern Ro2_YYYYMMDD
     listFieldCampains = [f for f in os.listdir(rawFileDir) if re.match(r'^Ro2_[0-9]{8}$', f)]
-    
+
     for iFieldCampain in listFieldCampains:
-        
+
         #Find folders that match the pattern Station_YYYYMMDD
         sationNameRegex=r'^' + stationName + r'_[0-9]{8}$'
         listDataCollection  = [f for f in os.listdir(os.path.join(rawFileDir,iFieldCampain)) if re.match(sationNameRegex, f)]
-        
+
         for iDataCollection in listDataCollection:
             print(iDataCollection)
-            for rawFile in os.listdir(os.path.join(rawFileDir,iFieldCampain,iDataCollection)):              
-                print('\t'+rawFile)  
-                
+            for rawFile in os.listdir(os.path.join(rawFileDir,iFieldCampain,iDataCollection)):
+                print('\t'+rawFile)
+
                 inFile=os.path.join(rawFileDir,iFieldCampain,iDataCollection,rawFile)
                 outFile=os.path.join(asciiOutDir,stationName,rawFile)
-                
-                # File type name handling           
+
+                # File type name handling
                 if bool(re.search("ts_data_",rawFile)) | bool(re.search("_Time_Series_",rawFile)):
-                    extension="_eddy.csv" 
+                    extension="_eddy.csv"
                 elif bool(re.search("alerte",rawFile)):
-                    extension="_alert.csv"         
+                    extension="_alert.csv"
                 elif bool(re.search("met30min",rawFile)) | bool(re.search("_Flux_CSIFormat_",rawFile)) | bool(re.search("flux",rawFile)):
-                    extension="_slow.csv" 
+                    extension="_slow.csv"
                 elif bool(re.search("radiation",rawFile)) | bool(re.search("_Flux_Notes_",rawFile)):
-                    extension="_slow2.csv"             
+                    extension="_slow2.csv"
                 else:                           # .cr1 / .cr3 / sys_log files / Config_Setting_Notes / Flux_AmeriFluxFormat_12
                     shutil.copy(inFile,outFile) # TODO solve issue: file with same name will overwrite
                     continue
-                
+
                 # Conversion from the Campbell binary file to csv format
                 # TODO check compatibility with unix and Wine
                 process=os.path.join(".\Bin","raw2ascii","csidft_convert.exe")
                 subprocess.call([process, inFile, outFile, 'ToA5'])
-                
+
                 # Rename file according to date
                 fileContent=pd.read_csv(outFile, sep=',', index_col=None, skiprows=[0,2,3], nrows=1)
                 try:
@@ -59,12 +60,12 @@ def convert_CSbinary_to_csv(stationName,rawFileDir,asciiOutDir):
                 shutil.move(outFile,os.path.join(asciiOutDir,stationName,newFileName))
 
 def batch_process_eddypro(iStation,asciiOutDir,eddyproConfig,eddyproMetaData,eddyproOutDir):
-              
+
     eddyproOutDir   = eddyproOutDir + iStation
     eddyproConfig   = eddyproConfig + "Ro2_" + iStation + ".eddypro"
     eddyproMetaData = eddyproMetaData +"Ro2_"+ iStation + ".metadata"
     asciiOutDir     = asciiOutDir + iStation
-    
+
     # Read in the Eddy Pro config file and replace target strings
     # TODO check if the path must be absolute
     with fileinput.FileInput(eddyproConfig, inplace=True, backup='.bak') as file:
@@ -95,9 +96,47 @@ def batch_process_eddypro(iStation,asciiOutDir,eddyproConfig,eddyproMetaData,edd
                 print(line,end='\n')
             else:
                 print(line,end='')
-            # TODO add line to modify dates + round up to next 30 minutes : dt.now() + (dt.min - dt.now()) % td(minutes=30)            
-            # TODO pr_subset=0 for "select a different period"    
-            
+            # TODO add line to modify dates + round up to next 30 minutes : dt.now() + (dt.min - dt.now()) % td(minutes=30)
+            # TODO pr_subset=0 for "select a different period"
+
     # TODO check compatibility with unix and Wine
-    process=os.path.join(".\Bin","EddyPro","eddypro_rp.exe")    
-    subprocess.call([process, eddyproConfig])    
+    process=os.path.join(".\Bin","EddyPro","eddypro_rp.exe")
+    subprocess.call([process, eddyproConfig])
+
+
+def merge_eddy_and_slow(iStation,asciiOutDir,eddyproOutDir,mergedCsvOutDir):
+
+    # TODO keep untits to csv
+
+    # Module to merge same type of slow data together
+    def merge_slow_data(slowList):
+        slow_df=pd.read_csv(os.path.join(asciiOutDir,iStation,slowList[0]), sep=',',skiprows=[0,2,3], nrows=0, low_memory=False) # Initialize columns name
+        for iSlow in slowList:
+            tmp_df=pd.read_csv(os.path.join(asciiOutDir,iStation,iSlow), sep=',',skiprows=[0,2,3], low_memory=False)
+            slow_df=slow_df.append(tmp_df)
+        slow_df.index=pd.to_datetime(slow_df.TIMESTAMP, yearfirst=True)
+        return slow_df
+
+    # List all slow csv files and merge them together
+    slowFileToLoad = os.listdir(os.path.join(asciiOutDir,iStation))
+    slowFileToLoad.sort()
+
+    # Slow data
+    slowList=[s for s in slowFileToLoad if re.match('.*slow\.csv', s)]
+    slow_df=merge_slow_data(slowList)
+
+    # Slow data 2
+    slowList2=[s for s in slowFileToLoad if re.match('.*slow2\.csv', s)]
+    if slowList2:
+        slow_df2=merge_slow_data(slowList2)
+        slow_df=pd.concat([slow_df, slow_df2], axis=1)
+
+    # Eddy file to load
+    eddyFileToLoad = glob(eddyproOutDir+'/'+iStation+'/'+'\*.csv') # TODO chose between glob and os.listdir
+    eddy_df=pd.read_csv(eddyFileToLoad[0], sep=',', index_col=None)
+    eddy_df.index=pd.to_datetime(eddy_df.date.map(str) +" "+ eddy_df.time.map(str), yearfirst=True)
+
+    # Merge and save
+    merged_df=pd.concat([eddy_df, slow_df], axis=1)
+    merged_df.to_csv(os.path.join(mergedCsvOutDir,iStation,"Merged_data.csv"))
+    # TODO understant non uniqueness of foret_ouest timestamps
