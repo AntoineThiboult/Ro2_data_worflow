d866bcf698ddf2473d355ab6aa70a3520cf0cb40
AntoineThiboult <38672758+AntoineThiboult@users.noreply.github.com>
2022-04-19 12:19:18 -0400

    Relax H and CO2_strg constraints

--- process_micromet/bandpass_filter.py
+++ process_micromet/bandpass_filter.py
@@ -15,7 +15,7 @@ def bandpass_filter(df, spiky_var):
     if spiky_var == 'LE':
         id_bandpass = ( df[spiky_var] < -35 ) | ( df[spiky_var] > 300 )     # in [W+1m-2]
     elif spiky_var == 'H':
-        id_bandpass = ( df[spiky_var] < -100 ) | ( df[spiky_var] > 400 )    # in [W+1m-2]
+        id_bandpass = ( df[spiky_var] < -150 ) | ( df[spiky_var] > 500 )    # in [W+1m-2]
     elif spiky_var == 'CO2_flux':
         id_bandpass = ( df[spiky_var] < -20 ) | ( df[spiky_var] > 20 )      # in [µmol+1s-1m-2]
     elif spiky_var == 'CH4_flux':
@@ -25,7 +25,7 @@ def bandpass_filter(df, spiky_var):
     elif spiky_var == 'H_strg':
         id_bandpass = ( df[spiky_var] < -50 ) | ( df[spiky_var] > 50 )    # in [W+1m-2]
     elif spiky_var == 'CO2_strg':
-        id_bandpass = ( df[spiky_var] < -5 ) | ( df[spiky_var] > 5 )      # in [µmol+1s-1m-2]
+        id_bandpass = ( df[spiky_var] < -8 ) | ( df[spiky_var] > 8 )      # in [µmol+1s-1m-2]
     elif spiky_var == 'CH4_strg':
         id_bandpass = ( df[spiky_var] < -0.05 ) | ( df[spiky_var] > 0.05 )   # in [µmol+1s-1m-2]
     return id_bandpass
