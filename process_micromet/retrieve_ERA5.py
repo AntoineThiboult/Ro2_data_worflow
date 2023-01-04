1e0d7f2ccb9dee603de6f2b581c940a962682905
AntoineThiboult <38672758+AntoineThiboult@users.noreply.github.com>
2023-01-04 17:55:37 -0500

    Make daily CDS request
    Copernicus Climate Data Center restricted the size of the request. It is not possible anymore to do a single request for a whole month (if several variables are requested).

--- process_micromet/retrieve_ERA5.py
+++ process_micromet/retrieve_ERA5.py
@@ -3,8 +3,49 @@ import cdsapi
 import os
 import time
 import pandas as pd
+from calendar import monthrange
 import concurrent.futures
 
+def retrieve_routine(variables, dataset, bounding_rectangle, ymd, delay):
+    """
+    Parameters
+    ----------
+    variables : list
+        Contains the list of variable to extract
+    dataset : dictionary
+        dictionary containing the keys 'name','dest_folder':'dest_folder',
+        'dest_subfolder', and 'product_type'
+    bounding_rectangle : list (4,)
+        Coordinates of the rectangle to extract (North, West, South, East)
+    ymd : Timestamp
+        Year Month Date to be extracted
+
+    Returns
+    -------
+    None.
+
+    """
+    cds = cdsapi.Client()
+    year = ymd.strftime('%Y')
+    month = ymd.strftime('%m')
+
+    for day in range(1,monthrange(ymd.year,ymd.month)[1]+1):
+        export_name = os.path.join(
+                    dataset['dest_folder'],
+                    dataset['dest_subfolder'],
+                    dataset['dest_subfolder'] + '_{}{}{:02}.nc'.format(year, month,day))
+        if not os.path.isfile(export_name):
+            time.sleep((delay%10)/2)
+            cds.retrieve(dataset['name'],
+                        {'variable': variables,
+                         'product_type': dataset['product_type'],
+                         'year':   year,
+                         'month':  month,
+                         'day':    day,
+                         'time':   ['{:02}:00'.format(f) for f in range(0,24)],
+                         'area':   bounding_rectangle,
+                         'format': 'netcdf'}, export_name
+                        )
 
 def retrieve_ERA5land(dates, dest_folder):
     """Retrieve data from the ECMWF Copernicus ERA5-land database in a netCDF4
@@ -32,46 +73,15 @@ def retrieve_ERA5land(dates, dest_folder):
 
     """
 
-    def retrieve_routine(variables, dest_folder, bounding_rectangle, ymd, delay):
-        """
-        Parameters
-        ----------
-        variables : list
-            Contains the list of variable to extract
-        dest_folder : string
-            Destination folder
-        bounding_rectangle : list (4,)
-            Coordinates of the rectangle to extract (North, West, South, East)
-        ymd : Timestamp
-            Year Month Date to be extracted
-
-        Returns
-        -------
-        None.
-
-        """
-        year = ymd.strftime('%Y')
-        month = ymd.strftime('%m')
-        export_name = os.path.join(
-            dest_folder,'ERA5','ERA5L_{}{}.nc'.format(year, month))
-
-        if not os.path.isfile(export_name):
-            time.sleep((delay%10)/2)
-            cds.retrieve('reanalysis-era5-land',
-                        {'variable': variables,
-                         'product_type': 'reanalysis',
-                         'year':   year,
-                         'month':  month,
-                         'day':    list(range(1,32)),
-                         'time':   ['{:02}:00'.format(f) for f in range(0,24)],
-                         'area':   bounding_rectangle,
-                         'format': 'netcdf'}, export_name
-                        )
     print('Start extracting ERA5 data...')
 
-    cds = cdsapi.Client()
+
 
     # Retrieval parameters
+    dataset = {'name':'reanalysis-era5-land',
+               'dest_folder':dest_folder,
+               'dest_subfolder':'ERA5L',
+               'product_type':'reanalysis'}
     datelist = pd.date_range(start=dates['start'], end=dates['end'], freq='M')
     bounding_rectangle = [53, -64.7, 50.2, -62.4 ] # North, West, South, East.
     variables = [
@@ -112,6 +122,6 @@ def retrieve_ERA5land(dates, dest_folder):
 
     # Process to retrieval
     with concurrent.futures.ThreadPoolExecutor(max_workers = 10) as executor:
-        [executor.submit(retrieve_routine, variables, dest_folder, bounding_rectangle, ymd, delay) for delay, ymd in enumerate(datelist)]
+        [executor.submit(retrieve_routine, variables, dataset, bounding_rectangle, ymd, delay) for delay, ymd in enumerate(datelist)]
 
     print('Done!\n')
