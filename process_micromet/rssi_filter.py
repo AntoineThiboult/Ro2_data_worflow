a256b675205d7d9bae8e48fba7f3635dcea52bcc
AntoineThiboult <38672758+AntoineThiboult@users.noreply.github.com>
2023-01-09 15:57:58 -0500

    Catch rssi exceptions
    Li 7500 doesn't provide explicitly rssi.

--- process_micromet/rssi_filter.py
+++ process_micromet/rssi_filter.py
@@ -14,14 +14,15 @@ def rssi_filter(df, var):
     -------
     id_rssi: index of low RSSI"""
 
-    if var in ['H','LE','LE_corr','H_corr','LE_strg','H_strg']:
-        id_low_rssi = df['rssi_H2O'] < 0.7
-    elif var in ['CO2_flux','CO2_flux_strg']:
-        id_low_rssi = df['rssi_CO2'] < 0.7
-    elif var in ['CH4_flux','CH4_flux_strg']:
-        id_low_rssi = df['rssi_CH4'] < 0.15
-    else:
-        warnings.warn(f'Unknown variable {var} for RSSI filtering')
+    try:
+        if var in ['H','LE','LE_corr','H_corr','LE_strg','H_strg']:
+            id_low_rssi = df['rssi_H2O'] < 0.7
+        elif var in ['CO2_flux','CO2_strg']:
+            id_low_rssi = df['rssi_CO2'] < 0.7
+        elif var in ['CH4_flux','CH4_strg']:
+            id_low_rssi = df['rssi_CH4'] < 0.15
+    except:
+        warnings.warn(f'Unable to filter {var} with RSSI')
         id_low_rssi = []
 
     return id_low_rssi
