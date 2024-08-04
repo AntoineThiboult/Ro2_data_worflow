819acb06291405b129f24605af0ec10c42199873
AntoineThiboult <38672758+AntoineThiboult@users.noreply.github.com>
2024-08-04 15:24:36 -0400

    Update merging technique
    Changed because of high memory usage. However, should be modified again because of loss of performance

--- process_micromet/merge_slow_csv.py
+++ process_micromet/merge_slow_csv.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
-import os
 import pandas as pd
-import re
+from pathlib import Path
+from tqdm import tqdm
+import warnings
+warnings.simplefilter('ignore', UserWarning)
 
-#TODO: check error message
-# C:\Users\anthi182\Documents\GitHub\Ro2_data_worflow\process_micromet\merge_slow_csv.py:35:
-# PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
-# slow_df.loc[id_slow_df_in_tmp_df,iVar] = \
-
-def merge_slow_csv(dates,stationName,asciiOutDir):
+def merge_slow_csv(dates,station_name,ascii_dir):
     """Merge slow csv data. Create a new column if the variable does not exist
     yet, or append if it does.
 
@@ -16,8 +13,8 @@ def merge_slow_csv(dates,stationName,asciiOutDir):
     ----------
     dates : dictionnary that contains a 'start' and 'end' dates
         Example: dates{'start': '2018-06-01', 'end': '2020-02-01'}
-    stationName: name of the station
-    asciiOutDir: path to the directory that contains the .csv files
+    station_name: name of the station
+    ascii_dir: path to the directory that contains the .csv files
 
     Returns
     -------
@@ -25,51 +22,73 @@ def merge_slow_csv(dates,stationName,asciiOutDir):
         measurement period
     """
 
-    print('Start merging slow data for station:', stationName, '...', end='\r')
-    # Module to merge same type of slow data together
-    def merge_slow_data(slow_df, slow_files):
+    print(f'Start merging slow data for station "{station_name}"...\n')
 
-        for iSlow in slow_files:
-            tmp_df = pd.read_csv(os.path.join(asciiOutDir,stationName,iSlow), sep=',',skiprows=[0,2,3], low_memory=False)
-            tmp_df['TIMESTAMP'] = pd.to_datetime(tmp_df['TIMESTAMP'])
-            tmp_df = tmp_df.drop_duplicates(subset='TIMESTAMP', keep='last')
-            id_tmp_in_slow_df = tmp_df['TIMESTAMP'].isin(slow_df['TIMESTAMP'])
-            id_slow_df_in_tmp_df = slow_df['TIMESTAMP'].isin(tmp_df['TIMESTAMP'])
-            col_names = tmp_df.columns[tmp_df.columns!='TIMESTAMP']
-            for iVar in col_names:
-                slow_df.loc[id_slow_df_in_tmp_df,iVar] = \
-                    tmp_df.loc[id_tmp_in_slow_df,iVar].values
-        return slow_df
-
-
-    # List all slow csv files and merge them together
-    fileInDir = os.listdir(os.path.join(asciiOutDir,stationName))
-    fileInDir.sort()
+    src_dir = Path(ascii_dir).joinpath(station_name)
+    slow_df = pd.DataFrame(index = pd.date_range(
+        start=dates['start'], end=dates['end'], freq='30min') )
 
     # Slow data
-    slow_files = [s for s in fileInDir if re.match('.*slow\.csv', s)]
-    slow_df = pd.DataFrame(
-        {'TIMESTAMP': pd.date_range(
-            start=dates['start'],end=dates['end'],freq='30min')})
-    slow_df = merge_slow_data(slow_df, slow_files)
+    slow_files = list(src_dir.glob('*slow.csv'))
+    slow_df = merge_dataframes(slow_df, slow_files)
 
     # Slow data 2
-    slow_files2 = [s for s in fileInDir if re.match('.*slow2\.csv', s)]
-    if slow_files2:
-        slow_df2 = pd.DataFrame(
-            {'TIMESTAMP': pd.date_range(
-                start=dates['start'],end=dates['end'],freq='30min')})
-        slow_df2 = merge_slow_data(slow_df2, slow_files2)
-        nonDuplicatedColumns = slow_df2.columns[~slow_df2.columns.isin(slow_df.columns)]
-        slow_df[nonDuplicatedColumns] = slow_df2[nonDuplicatedColumns]
+    slow_files2 = list(src_dir.glob('*slow2.csv'))
+    slow_df = merge_dataframes(slow_df, slow_files2)
 
     # Converts columns to float when possible
-    for iColumn in slow_df.columns:
+    for iColumn in slow_df.columns.difference(['TIMESTAMP']):
         try:
             slow_df.loc[:,iColumn] = slow_df.loc[:,iColumn].astype(float)
         except:
-            print('Could not convert {} to float'.format(iColumn))
+            warnings.warn(f'Could not convert {iColumn} to float')
 
     print('Done!')
 
     return slow_df
+
+
+def merge_dataframes(df_ref, file_list, merge_col='TIMESTAMP',
+                     preserve_index=True, verbose=True):
+    """
+    Combine dataframes. Add the columns to df_ref if they are present in the
+    files to merge.
+    May preserve index (preserve_index=True) or add extra index to df_ref
+    (preserve_index=False) if contained in files.
+
+    Parameters
+    ----------
+    df_ref : Reference Pandas Dataframe
+    file_list : List of files path to merge
+    merge_col: String, optional.
+        Name of the column used to merge Dataframes
+    preserve_index : Bool, optional
+        If true, the index of df_ref is preserved.
+        If false, additional index will be added if they are contained in the
+        files to merge.
+    verbose: Bool, optional
+        Use tqdm to display progress
+
+    Returns
+    -------
+    df_ref : Pandas Datagrame
+        Contains merged files
+    """
+
+    for i_slow_file in tqdm(file_list, disable=not verbose):
+        try:
+            tmp_df = pd.read_csv(i_slow_file, sep=',',skiprows=[0,2,3], low_memory=False)
+            tmp_df = tmp_df.drop_duplicates(subset='TIMESTAMP', keep='last')
+            tmp_df.index = pd.to_datetime(tmp_df['TIMESTAMP'])
+
+            if preserve_index:
+                tmp_df = tmp_df[tmp_df.index.isin(df_ref.index)]
+            if preserve_index & (len(tmp_df.index) == 0):
+                warnings.warn(f'File {i_slow_file} has no matching index')
+                continue
+
+            df_ref = df_ref.combine_first(tmp_df)
+        except Exception as e:
+            warnings.warn(f'An unexpected error occurred while processing file {i_slow_file}: {e}')
+
+    return df_ref
