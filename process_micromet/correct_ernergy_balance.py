aa7a21ebfe04e4eb336825146a8bedf153167daa
AntoineThiboult <38672758+AntoineThiboult@users.noreply.github.com>
2025-10-06 12:19:34 -0400

    Add default correction for water stations

--- process_micromet/correct_ernergy_balance.py
+++ process_micromet/correct_ernergy_balance.py
@@ -6,8 +6,9 @@ Created on Fri Apr  8 12:00:48 2022
 """
 import numpy as np
 
-def correct_energy_balance(df):
+def correct_energy_balance(df, corr_factor = None):
     """ Correct latent and sensible heat flux according to Mauder 2017
+    or with a constant correction factor.
 
     Parameters
     ----------
@@ -19,6 +20,9 @@ def correct_energy_balance(df):
             - H_strg
             - net_rad_CNR4
             - G
+    corr_factor : Float, default None.
+        Correction factor (constant) that, if specified, will be used to
+        correct the latent and sensible heat flux
 
     Returns
     -------
@@ -43,20 +47,23 @@ def correct_energy_balance(df):
     df['LE_corr_strg'] = df['LE_strg']
     df['H_corr_strg'] = df['H_strg']
 
+    if corr_factor: # Correction with specified constant
+        df['LE_corr'] = df['LE_corr'] * corr_factor
+        df['H_corr']  = df['H_corr'] * corr_factor
 
-    id_start = (df['timestamp'].dt.hour == 0).first_valid_index()
-    id_end = (df['timestamp'].dt.hour == 0).last_valid_index()
-
-    for i in range(id_start,id_end,48):
-        idx_bol = df.loc[i:i+47,'rad_shortwave_down_CNR4'] > 20
-        if any(idx_bol):
-            idx = idx_bol[idx_bol].index
-            C = (df.loc[idx,'H_gf_mds'].sum() + df.loc[idx,'LE_gf_mds'].sum()) / \
-                (-df.loc[idx,'rad_net_CNR4'].sum() + df.loc[idx,'G'].sum()
-                 + df.loc[idx,'LE_strg'].sum() + df.loc[idx,'H_strg'].sum())
-            if abs(C) < 0.2:
-                C = np.nan
-            df.loc[idx,'LE_corr'] = df.loc[idx,'LE_corr'] * -1/C
-            df.loc[idx,'H_corr']  = df.loc[idx,'H_corr'] * -1/C
+    else: # Mauder correction
+        id_start = (df.hour == 0).first_valid_index()
+        id_end = (df.hour == 0).last_valid_index()
+        for i in range(id_start,id_end,48):
+            idx_bol = df.loc[i:i+47,'rad_shortwave_down_CNR4'] > 20
+            if any(idx_bol):
+                idx = idx_bol[idx_bol].index
+                C = (df.loc[idx,'H_gf_mds'].sum() + df.loc[idx,'LE_gf_mds'].sum()) / \
+                    (-df.loc[idx,'rad_net_CNR4'].sum() + df.loc[idx,'G'].sum()
+                     + df.loc[idx,'LE_strg'].sum() + df.loc[idx,'H_strg'].sum())
+                if abs(C) < 0.2:
+                    C = np.nan
+                df.loc[idx,'LE_corr'] = df.loc[idx,'LE_corr'] * -1/C
+                df.loc[idx,'H_corr']  = df.loc[idx,'H_corr'] * -1/C
 
     return df
